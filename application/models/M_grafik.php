876554ed24a9353e691111cefd8cf32d7d7408da
dandiabil <45232854+dandiabil@users.noreply.github.com>
2020-07-02 17:17:32 +0700

    Update M_grafik.php

--- application/models/M_grafik.php
+++ application/models/M_grafik.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_grafik extends CI_Model{
-    function get_data_stok(){
+    function get_data_mhs(){
         $this->db->from('tbl_mhs');
         $this->db->select('nama_mhs', 'nilai');
         $this->db->select_sum('nilai');
